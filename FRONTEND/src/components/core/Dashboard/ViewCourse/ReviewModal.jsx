c355d72094533107bff01421518f055ab5f66850
tulsirijal2019@gmail.com <tulsirijal@2019>
2023-07-05 09:50:09 +0100

    small change

--- FRONTEND/src/components/core/Dashboard/ViewCourse/ReviewModal.jsx
+++ FRONTEND/src/components/core/Dashboard/ViewCourse/ReviewModal.jsx
@@ -7,20 +7,28 @@ import IconBtn from "../../../common/IconBtn";
 import { addRatingAndReview } from "../../../../services/operations/courseDetailsApi";
 export default function ReviewModal({ setReviewModal }) {
   const { user } = useSelector((state) => state.profile);
-  const {entireCourseData} = useSelector((state)=>state.viewCourse);
-  const {token} = useSelector((state)=>state.auth);
+  const { entireCourseData } = useSelector((state) => state.viewCourse);
+  const { token } = useSelector((state) => state.auth);
   const {
     register,
     formState: { errors },
     handleSubmit,
-    setValue
+    setValue,
   } = useForm();
-  function ratingChanged(newRating){
-    console.log('rating', newRating);
-    setValue('courseRating',newRating);
+  function ratingChanged(newRating) {
+    console.log("rating", newRating);
+    setValue("courseRating", newRating);
   }
   async function onSubmit(data) {
-   await  addRatingAndReview({courseId:entireCourseData._id,rating:data.courseRating,reviews:data.review},token);
+    await addRatingAndReview(
+      {
+        courseId: entireCourseData._id,
+        rating: data.courseRating,
+        reviews: data.review,
+      },
+      token
+    );
+    setReviewModal(false);
   }
   return (
     <div className="z-[9999] fixed inset-0 backdrop-blur-sm flex items-center justify-center">
@@ -44,15 +52,25 @@ export default function ReviewModal({ setReviewModal }) {
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col items-center justify-center"
         >
-          <ReactStars count={5} activeColor="#ffd700" size={24} onChange={ratingChanged} />
+          <ReactStars
+            count={5}
+            activeColor="#ffd700"
+            size={24}
+            onChange={ratingChanged}
+          />
           <div className="self-start  flex flex-col  px-5 py-2 gap-3 w-full bg-richblack-800">
             <label className="text-richblack-5 text-sm px-4 self-start">
               Add your experience <sup className="text-pink-50">*</sup>{" "}
             </label>
-            <textarea name="review" id="review" className="w-11/12 h-[100px] bg-richblack-700 self-center outline-none text-richblack-5 px-2"
-             {...register("review",{required:true})}
-             />
-             {errors.review && <span className="text-xs text-yellow-50">Review is required</span>}
+            <textarea
+              name="review"
+              id="review"
+              className="w-11/12 h-[100px] bg-richblack-700 self-center outline-none text-richblack-5 px-2"
+              {...register("review", { required: true })}
+            />
+            {errors.review && (
+              <span className="text-xs text-yellow-50">Review is required</span>
+            )}
           </div>
           <div className="self-end flex items-center gap-3 px-5 py-4">
             <button
@@ -62,7 +80,7 @@ export default function ReviewModal({ setReviewModal }) {
             >
               Cancel
             </button>
-            <IconBtn type='submit' text='Save' />
+            <IconBtn type="submit" text="Save" />
           </div>
         </form>
       </div>
