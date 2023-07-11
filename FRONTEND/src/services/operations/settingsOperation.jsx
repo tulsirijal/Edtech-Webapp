f9a6f8b470c1eda5ff68cd2029abc5425db37d59
tulsirijal2019@gmail.com <tulsirijal@2019>
2023-07-11 17:17:14 +0100

    Dispatch the setUser function with the updated user and user image

--- FRONTEND/src/services/operations/settingsOperation.jsx
+++ FRONTEND/src/services/operations/settingsOperation.jsx
@@ -1,3 +1,4 @@
+import { setUser } from "../../slices/profileSlice";
 import { apiConnector } from "../apiConnector";
 import { userDetails } from "../apis";
 import toast from "react-hot-toast";
@@ -5,12 +6,13 @@ export function updateDisplayPicture(formData, token, setLoading) {
   return async (dispatch) => {
     try {
       setLoading(true);
-      await apiConnector("PUT", userDetails.UPDATE_PROFILE_PICTURE, formData, {
+     const response =  await apiConnector("PUT", userDetails.UPDATE_PROFILE_PICTURE, formData, {
         "Content-Type": "multipart/form-data",
         Authorization: `Bearer ${token}`,
       });
       toast.success("Successfully updated");
       setLoading(false);
+      dispatch(setUser(response.data.data))
     } catch (error) {
       console.log(error);
       setLoading(false);
