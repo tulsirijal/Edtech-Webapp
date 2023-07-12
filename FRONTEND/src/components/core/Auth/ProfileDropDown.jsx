829ba6feb3383da7557e987c2985a24f6d8eecbd
tulsirijal2019@gmail.com <tulsirijal@2019>
2023-07-12 14:01:56 +0100

    Made the profile dropdown dynamic with the sidebarLink component.

--- FRONTEND/src/components/core/Auth/ProfileDropDown.jsx
+++ FRONTEND/src/components/core/Auth/ProfileDropDown.jsx
@@ -5,9 +5,11 @@ import { MdOutlineLogout } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { logout } from "../../../services/operations/authServices";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import {VscSettingsGear,VscMortarBoard,VscHistory} from 'react-icons/vsc'
-import {RxHamburgerMenu} from 'react-icons/rx'
+import { Link, useNavigate } from "react-router-dom";
+import { VscSettingsGear, VscDashboard } from "react-icons/vsc";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { sidebarLinks } from "../../../data/dashboard-links";
+import SidebarLink from "../Dashboard/SidebarLink";
 export default function ProfileDropDown() {
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.profile);
@@ -21,6 +23,7 @@ export default function ProfileDropDown() {
   function handleShowDropdown() {
     setShowDropdown((prev) => !prev);
   }
+
   return (
     <div
       className="flex relative gap-1 z-[999] items-center"
@@ -28,27 +31,24 @@ export default function ProfileDropDown() {
     >
       <img className="w-7 rounded-full" src={user?.image} />
       <IoMdArrowDropdown className="hidden md:block" />
-      <RxHamburgerMenu className='md:hidden block mt-1 ml-1'/>
+      <RxHamburgerMenu className="md:hidden block mt-1 ml-1" />
       <div
         className={`${
           showDropdown ? "block" : "hidden"
         } z-[999] absolute top-[120%] -left-10 border border-richblack-700 rounded-md bg-richblack-800 text-richblack-100`}
       >
-        <div className="flex items-center gap-1 p-2 border-b-[1px] border-b-richblack-700 hover:bg-richblack-700 transition-all duration-200">
-          <AiFillDashboard />
-          <p onClick={() => navigate("/dashboard/my-profile")}>dashboard</p>
+        <div className="md:hidden">
+          {sidebarLinks.map((link, index) => {
+            if (link.type && user?.accountType !== link.type) return null;
+            return <SidebarLink link={link} key={index} />;
+          })}
         </div>
-       
-        <div className="md:hidden flex items-center gap-1 p-2 border-b-[1px] border-b-richblack-700 hover:bg-richblack-700 transition-all duration-200">
-          <VscMortarBoard/>
-          <p onClick={() => navigate("/dashboard/enrolled-courses")}>
-             Courses
+        <Link to="/dashboard/my-profile" className="hidden md:block">
+          <p className="text-richblack-100 flex items-center gap-1 p-2 z-[999] border-b-[1px] border-b-richblack-700 hover:bg-richblack-700 transition-all duration-200">
+            <VscDashboard />
+            <span>Dashboard</span>
           </p>
-        </div>
-        <div className="md:hidden flex items-center gap-1 p-2 border-b-[1px] border-b-richblack-700 hover:bg-richblack-700 transition-all duration-200">
-          <VscHistory />
-          <p onClick={() => navigate("/dashboard/purchase-history")}>Cart</p>
-        </div>
+        </Link>
         <div className="md:hidden flex items-center gap-1 p-2 border-b-[1px] border-b-richblack-700 hover:bg-richblack-700 transition-all duration-200">
           <VscSettingsGear />
           <p onClick={() => navigate("/dashboard/settings")}>Settings</p>
