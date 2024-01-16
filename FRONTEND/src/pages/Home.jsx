07774c031c52e00f22e5248986ee161be6377736
tulsirijal2019@gmail.com <tulsirijal@2019>
2024-01-16 12:27:01 +0000

    Add a loader

--- FRONTEND/src/pages/Home.jsx
+++ FRONTEND/src/pages/Home.jsx
@@ -86,7 +86,7 @@ export default function Home() {
                   </Link>
                 ))
               ) : (
-                <div></div>
+                <span className="loader"></span>
               )}
             </div>
           </div>
