20c02388403e7b9b27c49ecd737afe13174494e9
tulsirijal2019@gmail.com <tulsirijal@2019>
2026-01-28 10:13:44 +0000

    installed the latest version of axios

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,14 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   define: {
-
     'global': 'globalThis',
     'process.env': {},
   },
   build: {
     sourcemap: true,
-
     commonjsOptions: {
       transformMixedEsModules: true,
     }
