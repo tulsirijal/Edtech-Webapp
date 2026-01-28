0e2ebbe02634277fb1da35715a360e378f9bfcff
tulsirijal2019@gmail.com <tulsirijal@2019>
2026-01-28 10:10:39 +0000

    fix axios error

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define:{
-    global:'globalThis',
+  define: {
+
+    'global': 'globalThis',
+    'process.env': {},
   },
-  build: { sourcemap: true }
+  build: {
+    sourcemap: true,
+
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  }
 })
