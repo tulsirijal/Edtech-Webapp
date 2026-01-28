7a1e7ccc8536f047dd78c55ee7806aea42d9b797
tulsirijal2019@gmail.com <tulsirijal@2019>
2026-01-28 09:27:02 +0000

    Minor bug fixes

--- FRONTEND/src/services/apis.js
+++ FRONTEND/src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:4000/api/v1";
 export const categories = {
     CATEGORIES_API:BASE_URL + "/course/showAllCategories",
 }
