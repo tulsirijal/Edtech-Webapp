ba3bd01c748f7c5f72aa1596610073aa7ddc2047
tulsirijal2019@gmail.com <tulsirijal@2019>
2026-01-28 12:00:56 +0000

    globalthis change in viteconfig

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,23 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
- 
-    'global': 'window',
-    'process.env': {},
-  },
-  resolve: {
-    alias: {
-
-      'global': 'window',
-    },
+  define:{
+    globalThis:"window"
   },
   build: {
     sourcemap: true,
     commonjsOptions: {
       transformMixedEsModules: true,
-    }
-  }
-})
+    },
+  },
+});
